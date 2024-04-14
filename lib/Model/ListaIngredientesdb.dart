207b5455708c61797e822719a27760c857696af3
Marlon Alfredo Garcia Meza <marlon_alfredo@outlook.com>
2024-04-13 20:49:23 -0600

    Modificaciones en el apartado de lista ingredientes

--- lib/Model/ListaIngredientesdb.dart
+++ lib/Model/ListaIngredientesdb.dart
@@ -1,16 +1,25 @@
 class ListIng {
   int idList;
-  int idIng;
+  String nombre;
   int cantidad;
+  int idIng;
+  int idreceta;
 
-  ListIng({required this.idList, required this.idIng, required this.cantidad});
+  ListIng(
+      {required this.idList,
+      required this.nombre,
+      required this.cantidad,
+      required this.idIng,
+      required this.idreceta});
 
   // Método para convertir un objeto Receta a un mapa
   Map<String, dynamic> toMap() {
     return {
       'id_lis_ing': idList,
+      'nombre_ingrediente': nombre,
+      'cantidad_lis_ing': cantidad,
       'id_ing': idIng,
-      'cantidad_lis_ing': cantidad
+      'id_recetas': idreceta
     };
   }
 }
--- lib/Model/M_ListaIng.dart
+++ lib/Model/M_ListaIng.dart
@@ -6,15 +6,4 @@ class MLista {
   final DRLista _repository;
 
   MLista(Database database) : _repository = DRLista(database);
-
-  // Método para agregar la cantidad de un ingrediente
-  Future<void> agregarCantidadIngrediente(
-      ListIng listaIng, int cantidad) async {
-    await _repository.agregarCantidadIngrediente(listaIng, cantidad);
-  }
-
-  // Método para quitar la cantidad de un ingrediente
-  Future<void> quitarCantidadIngrediente(ListIng listaIng, int cantidad) async {
-    await _repository.quitarCantidadIngrediente(listaIng, cantidad);
-  }
 }
